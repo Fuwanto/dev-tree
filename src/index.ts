8994576915177ef3f3c6f773348092c33172b1c9
Antonella Denisse Cuenca <fuwa08@hotmail.com>
2025-02-26 21:13:04 -0300

    add MongoDB connection and enhance logging with colors

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,8 @@
+import colors from "colors"
 import app from "./server"
 
 const port = process.env.PORT || 4000
 
 app.listen(port, () => {
-  console.log("Servidor Funcionando en el puerto", port)
+  console.log(colors.magenta(`Servidor Funcionando en el puerto: ${port}`))
 })
