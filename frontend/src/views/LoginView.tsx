2282178ba206e37254a41e2dc1e0a2f343c92a1d
Antonella Denisse Cuenca <fuwa08@hotmail.com>
2025-03-03 17:44:10 -0300

    add axios configuration, user types, CORS setup, and authentication layout; include social media icons

--- frontend/src/views/LoginView.tsx
+++ frontend/src/views/LoginView.tsx
@@ -3,9 +3,14 @@ import { Link } from "react-router"
 export default function LoginView() {
   return (
     <>
-      <h1>Login</h1>
-      <nav>
-        <Link to="/auth/register">¿No tienes cuenta? Regístrate aquí</Link>
+      <h1 className="text-4xl text-white font-bold">Iniciar Sesión</h1>
+      <nav className="mt-10">
+        <Link
+          className="text-center text-white text-lg block"
+          to="/auth/register"
+        >
+          ¿No tienes cuenta? Regístrate aquí
+        </Link>
       </nav>
     </>
   )
